94d43dc798fa75b7b8b8331debeb77246bf628ff
hanzo7656-prog <hanzo7656@gmail.com>
2025-09-24 17:58:17 +0330

    Update Vortex_Code_2.py

--- Vortex_Code_2.py
+++ Vortex_Code_2.py
@@ -1179,8 +1179,7 @@ class StreamlitUI:
                          delta_color=pnl_color)
             with clo4:
                 if st.button("بروزرسانی قیمت‌ها"):
-                    st.return()
-                        
+                    st.return()   
             # Assets table
             st.subheader("📋 دارایی‌های پرتفوی")
             assets_df = pd.DataFrame(portfolio_value['assets'])
